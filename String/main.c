4dea9e903f6a001c00c9fa9a838998046f13c696
ZahwaAli00 <azzamzahwa00@gmail.com>
2026-05-08 16:59:52 +0300

    C Program to Compare Two Strings

--- String/main.c
+++ String/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define n 15
+#define n 30
 int main()
 {
     char str1[n];
@@ -10,7 +10,14 @@ int main()
     gets(str1);
     printf("please enter string 2: ");
     gets(str2);
-    strcat(str1,str2);
-    printf("%s", str1);
+    if(strcmp(str1,str2)>0){
+        printf("string 1 is greater than string 2");
+    }
+    else if (strcmp(str1,str2)<0){
+        printf("string 1 is less than string 2");
+    }
+    else if(strcmp(str1,str2)==0){
+        printf("string 1 is  equal to string 2");
+    }
     return 0;
 }
