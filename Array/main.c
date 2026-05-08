cc598e6d0c63ddb016f7d607c3c83931f5bc3bfc
ZahwaAli00 <azzamzahwa00@gmail.com>
2026-05-08 16:51:29 +0300

    C Program to Remove All Occurrences of an Element in an Array

--- Array/main.c
+++ Array/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define n 6
+
 int main()
 {
-    int num,rem;
-    printf(" please enter a number\n");
-    scanf("%d",&num);
-    int arry[10];
-    int count=0;
-    int original_num =num;
-    while(num != 0) {
-            num = num / 10;
-            count++;
+    int array[n];
+    int a;
+
+    for(int i=0; i<n; i++){
+        printf("please enter element %d: ", i+1);
+        scanf("%d", &array[i]);
+    }
+
+    printf("enter the element that you want to remove: ");
+    scanf("%d", &a);
+
+    for(int i=0; i<n; i++){
+        if(array[i] != a){
+            printf("%d ", array[i]);
         }
-    num= original_num;
-    for(int i=0;i<count;i++){
-            rem=num%10;
-           num=num/10;
-          arry[i]=rem;
     }
-    for(int i=0;i<count;i++){
-        printf("%d",arry[i]);
-    }
+
     return 0;
 }
