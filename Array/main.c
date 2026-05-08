71fac487d4b7b5dd0d7376ecaaa2b6ab2ed0a7df
ZahwaAli00 <azzamzahwa00@gmail.com>
2026-05-08 16:50:51 +0300

    C Program to Reverse a Number

--- Array/main.c
+++ Array/main.c
@@ -3,20 +3,24 @@
 
 int main()
 {
-    do{
-        int n;
-        int result;
-        printf("please enter n of fibonacci(n) :number of terms\n ");
-        scanf("%d",&n);
-        int arr[50];
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
+    int num,rem;
+    printf(" please enter a number\n");
+    scanf("%d",&num);
+    int arry[10];
+    int count=0;
+    int original_num =num;
+    while(num != 0) {
+            num = num / 10;
+            count++;
         }
-        for(int i=0;i<=n;i++){
-            printf("%d ",arr[i]);
-        }
-    }while(1);
+    num= original_num;
+    for(int i=0;i<count;i++){
+            rem=num%10;
+           num=num/10;
+          arry[i]=rem;
+    }
+    for(int i=0;i<count;i++){
+        printf("%d",arry[i]);
+    }
     return 0;
 }
