c2527989c92db4fedd48a0f808c45b2485a391e2
ZahwaAli00 <azzamzahwa00@gmail.com>
2026-05-08 16:50:17 +0300

    C Program to Print Fibonacci Series

--- Array/main.c
+++ Array/main.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define a 5
-#define b 3
+
 int main()
 {
-    int arr1[a];
-    int arr2[b];
-    int arr3[a+b];
-    for(int i=0;i<a;i++){
-        printf("please enter array %d 'st element of first array ",i+1);
-        scanf("%d",&arr1[i]);
-    }
-    printf("\n");
-     for(int i=0;i<b;i++){
-        printf("please enter array %d 'st element of second array ",i+1);
-        scanf("%d",&arr2[i]);
-    }
-    for(int i=0;i<a;i++){
-        arr3[i]=arr1[i];
-    }
-    for(int i=a;i<(a+b);i++){
-        arr3[i]=arr2[i-a];
-    }
-     printf("the merge: ");
-    for(int i=0;i<a+b;i++){
-        printf("%d ",arr3[i]);
-    }
-
+    do{
+        int n;
+        int result;
+        printf("please enter n of fibonacci(n) :number of terms\n ");
+        scanf("%d",&n);
+        int arr[50];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        for(int i=0;i<=n;i++){
+            printf("%d ",arr[i]);
+        }
+    }while(1);
     return 0;
 }
